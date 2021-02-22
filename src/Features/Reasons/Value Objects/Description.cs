dc83e0928fe8dccebd60730a0609aac397bd6545
Sam Charette <sam.charette@greatersudbury.ca>
2021-02-22 08:01:28 -0500

    Added reason object with description, probability and weight value objects.

---
+++ src/Features/Reasons/Value Objects/Description.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace procon.Features.Reasons.Value_Objects
+{
+    public class Description
+    {
+        [MaxLength(250)] private readonly string _text;
+
+        public Description(string description)
+        {
+            _text = description;
+        }
+
+        public override string ToString() => _text;
+    }
+}
---
+++ src/Features/Reasons/Value Objects/Weight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace procon.Features.Reasons.Value_Objects
+{
+    public class Weight
+    {
+        private readonly int _weight;
+
+        public Weight(int weight)
+        {
+            var absoluteWeight = Math.Abs(weight);
+            if (absoluteWeight > 0 && absoluteWeight < 101)
+            {
+                _weight = weight;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(weight),
+                    "Must be between 1 and 100, or -1 and -100");
+            }
+        }   
+
+        public int Value() => _weight;
+    }
+}
